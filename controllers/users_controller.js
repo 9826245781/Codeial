e51944df9b29452a453cf274f6e01d0a7135a1cf
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2022-02-09 15:51:02 +0545

    manual wala

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,18 +1,35 @@
+// const user = require('../models/user');
 const User=require('../models/user');
 
+
+
+
 module.exports.profile=function(req,res){
-
-return res.render('user_profile',{
-title:"Profile"
+if(req.cookies.user_id){
+User.findById(req.cookies.user_id,function(err,user){
+    if(err){
+        console.log("Error in finding user in signing up");
+        return;
+    }
+    if(user){
+        return res.render('user_profile',{
+            title:"Profile",
+             user:user
+            });
+    }
+    return res.redirect('/users/sign-in')
 });
-
+}
+else{
+    return res.redirect('/users/sign-in')
+}
 }
 //render sign up page
 
 module.exports.signUp=function(req,res){
     return res.render('user_signup',{
         title:"Codeial | Signup"
-    })
+    });
 }
 
 //render sign in page
@@ -20,7 +37,17 @@ module.exports.signIn=function(req,res){
     return res.render('user_signin',{
         title:"Codeial | Signin"
     })
+    
 }
+//sign out
+module.exports.signOut=function(req,res){
+  
+  res.cookie('user_id',678 ); 
+     return res.redirect('/users/sign-in')
+}
+
+
+
 
 // get the sign up data
 module.exports.create=function(req,res){
@@ -52,10 +79,28 @@ User.findOne({email:req.body.email},function(err,user){
 
 
 module.exports.createSession=function(req,res){
+//find the user
+User.findOne({email : req.body.email},function(err,user){
+    if(err) console.log("User not found in signing in");
+//handle user found
 
-    
+if(user){
+//handle mismatching pass which dont match
+if(user.password!=req.body.password){
+    return res.redirect('back');
+}
+
+//handle session creation
+res.cookie('user_id',user.id);
+return res.redirect('/users/profile');
 
 
 
-    
+}else{
+//handle user not found
+return res.redirect('back');
+}
+
+});
+
 }
